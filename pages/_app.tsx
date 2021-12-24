1871cabdf169ea8507f075cd7cfdefbf04341c67
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-12-24 14:25:12 +0100

    Add mantine.dev, add header and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      theme={{
+        // Override any other properties from default theme
+        fontFamily: 'Open Sans, DejaVu Sans, sans serif',
+        spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+        colorScheme: 'dark',
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
 export default MyApp;
