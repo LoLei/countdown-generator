416348e96edb463303c65887bd181851597c4c2c
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-12-24 15:41:53 +0100

    Add styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import {
   Card,
   Container,
   createStyles,
-  Paper,
+  Group,
   Text,
   Title,
 } from '@mantine/core';
@@ -28,7 +28,32 @@ const useStyles = createStyles({
     minHeight: '1rem',
     textAlign: 'center',
   },
+  titleNew: {
+    textAlign: 'center',
+  },
+  titleOld: {
+    textAlign: 'center',
+  },
 });
+
+interface ICountdown {
+  dateCreated: Date;
+  dateDue: Date;
+}
+
+/**
+ * Mock data for now
+ */
+const otherCountdowns: ICountdown[] = [
+  {
+    dateCreated: new Date('2011-04-11T10:20:30Z'),
+    dateDue: new Date('2011-04-12T10:20:30Z'),
+  },
+  {
+    dateCreated: new Date('2021-04-11T10:20:30Z'),
+    dateDue: new Date('2012-04-12T10:20:30Z'),
+  },
+];
 
 const Home: NextPage = () => {
   const { classes } = useStyles();
@@ -45,9 +70,29 @@ const Home: NextPage = () => {
 
       <main className={classes.main}>
         <Container>
-          <Card>Test Card</Card>
-          <Button>Create Countdown</Button>
-          <Paper>Test Paper</Paper>
+          <Group position="center" direction="column" grow>
+            <Card shadow="sm" padding="sm">
+              <Group position="center" direction="column" grow spacing="xs">
+                <Title className={classes.titleNew} order={4}>
+                  New Countdown
+                </Title>
+                <Button>Create Countdown</Button>
+              </Group>
+            </Card>
+            <Card shadow="sm" padding="sm">
+              <Title className={classes.titleOld} order={4}>
+                Old Countdowns
+              </Title>
+              {otherCountdowns.map((it, idx) => {
+                return (
+                  <Text key={idx}>
+                    Created: {it.dateCreated.toDateString()}, due:{' '}
+                    {it.dateDue.toDateString()}
+                  </Text>
+                );
+              })}
+            </Card>
+          </Group>
         </Container>
       </main>
 
