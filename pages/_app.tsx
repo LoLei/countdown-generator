6c1439624a2c64b35c07c7e9e5853110bb9b60ee
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-12-24 15:02:48 +0100

    Add _document for font CDN

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <MantineProvider
       theme={{
         // Override any other properties from default theme
-        fontFamily: 'Open Sans, DejaVu Sans, sans serif',
+        fontFamily: 'Roboto, Open Sans, DejaVu Sans, sans serif',
+        fontFamilyMonospace: 'Monaco, Courier, monospace',
+        headings: { fontFamily: 'Roboto Condensed, sans-serif' },
         spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
         colorScheme: 'dark',
       }}
