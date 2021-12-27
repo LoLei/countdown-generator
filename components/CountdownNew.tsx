de069ad890096b14ef706b41fecdc5c437d8c8d7
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-12-27 09:22:32 +0100

    Add date picker

--- components/CountdownNew.tsx
+++ components/CountdownNew.tsx
@@ -1,5 +1,6 @@
-import { Button, Card, createStyles, Group, Title } from '@mantine/core';
-import React from 'react';
+import { Button, Card, createStyles, Group, Modal, Title } from '@mantine/core';
+import { DatePicker } from '@mantine/dates';
+import React, { useState } from 'react';
 
 const useStyles = createStyles({
   titleNew: {
@@ -8,19 +9,43 @@ const useStyles = createStyles({
 });
 
 const CountdownNew = (): JSX.Element => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [dateValue, onDateValueChange] = useState<Date | undefined>(new Date());
   const { classes } = useStyles();
 
   return (
-    <Card shadow="sm" padding="sm">
-      <Group position="center" direction="column" spacing="xs">
-        <Title className={classes.titleNew} order={4}>
-          New Countdown
-        </Title>
-        <Button variant="gradient" gradient={{ from: 'orange', to: 'red' }}>
-          Create Countdown
-        </Button>
-      </Group>
-    </Card>
+    <>
+      <Card shadow="sm" padding="sm">
+        <Group position="center" direction="column" spacing="xs">
+          <Title className={classes.titleNew} order={4}>
+            New Countdown
+          </Title>
+          <Button
+            variant="gradient"
+            gradient={{ from: 'orange', to: 'red' }}
+            onClick={() => setModalOpen(true)}
+          >
+            Create Countdown
+          </Button>
+        </Group>
+      </Card>
+      <Modal
+        opened={modalOpen}
+        onClose={() => setModalOpen(false)}
+        title="Create a new countdown"
+        centered
+      >
+        <DatePicker
+          value={dateValue}
+          onChange={(d) => onDateValueChange(d || undefined)}
+          label="Due date"
+          minDate={new Date()}
+          required
+          clearable={false}
+        />
+        <Button variant="light">Create</Button>
+      </Modal>
+    </>
   );
 };
 
