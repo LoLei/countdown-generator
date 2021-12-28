ad6c4261fde7b2aa7a06ed952b2b5dd0bb6ad760
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-12-28 10:24:31 +0100

    Display error if due date is before current date

--- components/CountdownNew.tsx
+++ components/CountdownNew.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, Group, Modal, Title } from '@mantine/core';
 import { DatePicker, TimeInput } from '@mantine/dates';
+import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
 import { AiOutlineCalendar, AiOutlineClockCircle } from 'react-icons/ai';
 
@@ -7,15 +8,34 @@ const CountdownNew = (): JSX.Element => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [dateValue, onDateValueChange] = useState<Date | undefined>(new Date());
   const [timeValue, onTimeValueChange] = useState<Date | undefined>(new Date());
-  const [timeError, setTimeError] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const errorMessage = 'Due date cannot be before current date';
+  const [combinedDueDate, setCombinedDueDate] = useState<Date | undefined>();
 
   useEffect(() => {
+    setError('');
+
     const dateNow = new Date();
     console.log({ dateNow });
     console.log({ dateValue });
     console.log({ timeValue });
-    // TODO: Set error if date & time is before now
+
+    const combined = dayjs(dateValue)
+      .add(timeValue?.getHours() || 0, 'hour')
+      .add(timeValue?.getMinutes() || 0, 'minute')
+      .add(timeValue?.getSeconds() || 0, 'second');
+
+    if (combined.isBefore(dateNow)) {
+      setError(errorMessage);
+    } else {
+      setCombinedDueDate(combined.toDate());
+    }
   }, [dateValue, timeValue]);
+
+  const onSubmitCreation = () => {
+    console.log({ combinedDueDate });
+    // TODO
+  };
 
   return (
     <>
@@ -53,10 +73,10 @@ const CountdownNew = (): JSX.Element => {
           required
           icon={<AiOutlineClockCircle />}
           withSeconds
-          invalid={timeError !== ''}
-          error={timeError}
+          invalid={error !== ''}
+          error={error}
         />
-        <Button variant="light" color="orange">
+        <Button variant="light" color="orange" onClick={onSubmitCreation}>
           Create
         </Button>
       </Modal>
