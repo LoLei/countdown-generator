8dd9e8f5a5e3174fba340b5285d47513d79f6556
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-12-28 14:09:38 +0100

    Fix automatic zoom on mobile for inputs with fontsize <16px

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,52 +2,66 @@ import { Global, MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider
-      theme={{
-        // Override any other properties from default theme
-        fontFamily: 'Roboto, Open Sans, DejaVu Sans, sans serif',
-        fontFamilyMonospace: 'Monaco, Courier, monospace',
-        headings: { fontFamily: 'Roboto Condensed, sans-serif' },
-        spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
-        colorScheme: 'dark',
-      }}
-    >
-      <Global
-        styles={(theme) => ({
-          'html, body': {
-            padding: 0,
-            margin: 0,
-            fontFamily: 'Roboto, Open Sans, DejaVu Sans, sans-serif',
-          },
-          body: {
-            ...theme.fn.fontStyles(),
-            backgroundColor:
-              theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-            color:
-              theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-            lineHeight: theme.lineHeight,
-          },
-          '#__next': {
-            minHeight: '100vh',
-            display: 'grid',
-            gridTemplateRows: 'auto 1fr auto',
-          },
-          a: {
-            color: 'inherit',
-            textDecoration: 'none',
-          },
-          '*, *::before, *::after': {
-            boxSizing: 'border-box',
-          },
-        })}
-      />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </MantineProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+
+      <MantineProvider
+        theme={{
+          // Override any other properties from default theme
+          fontFamily: 'Roboto, Open Sans, DejaVu Sans, sans serif',
+          fontFamilyMonospace: 'Monaco, Courier, monospace',
+          headings: { fontFamily: 'Roboto Condensed, sans-serif' },
+          spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+          colorScheme: 'dark',
+        }}
+      >
+        <Global
+          styles={(theme) => ({
+            'html, body': {
+              padding: 0,
+              margin: 0,
+              fontFamily: 'Roboto, Open Sans, DejaVu Sans, sans-serif',
+            },
+            body: {
+              ...theme.fn.fontStyles(),
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[7]
+                  : theme.white,
+              color:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[0]
+                  : theme.black,
+              lineHeight: theme.lineHeight,
+            },
+            '#__next': {
+              minHeight: '100vh',
+              display: 'grid',
+              gridTemplateRows: 'auto 1fr auto',
+            },
+            a: {
+              color: 'inherit',
+              textDecoration: 'none',
+            },
+            '*, *::before, *::after': {
+              boxSizing: 'border-box',
+            },
+          })}
+        />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
+    </>
   );
 }
 
